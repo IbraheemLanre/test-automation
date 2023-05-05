c796b9a9f9d8c69c26bc0b5d0ba4ccf24284107b
ibraheemlanre <talk2lanre205@yahoo.com>
2023-05-05 21:42:22 +0300

    made a git repo

---
+++ cypress/e2e/subscribe.cy.ts
@@ -0,0 +1,23 @@
+describe("Newsletter Subscribe Form", ()=>{
+    beforeEach(()=>{
+        cy.visit("http://localhost:3000")
+    })
+
+    it("allows users to subscribe to the email list", ()=>{
+        cy.getByData("email-input").type("johndoe@johndoe.com", {force: true})
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message").should('exist').contains("johndoe@johndoe.com")
+    })
+
+    it("does not allow invalid email address", ()=>{
+        cy.getByData("email-input").type("johndoe",{force:true})
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message").should("not.exist")
+    })
+
+    it("user already exist", ()=>{
+        cy.getByData("email-input").type("john@example.com")
+        cy.getByData("submit-button").click()
+        cy.getByData("server-error-message").should('exist').contains('already exists. Please use a different email address')
+    })
+})
