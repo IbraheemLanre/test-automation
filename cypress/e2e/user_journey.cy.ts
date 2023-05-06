096e29c67ab55271572663fe57f3123fb74f8b18
ibraheemlanre <talk2lanre205@yahoo.com>
2023-05-06 11:24:05 +0300

    completed the user journey code

--- cypress/e2e/user_journey.cy.ts
+++ cypress/e2e/user_journey.cy.ts
@@ -5,6 +5,10 @@ describe("User Journey", ()=>{
         cy.getByData("course-0").find("a").eq(3).click()
         cy.location("pathname").should("eq", "/testing-your-first-application")
         cy.getByData("next-lesson-button").click()
+        cy.location("pathname").should("eq", "/testing-your-first-application/app-install-and-overview")
+        cy.getByData("challenge-answer-0").click()
+        cy.getByData("next-lesson-button").should("exist").click()
+        cy.location("pathname").should("eq", "/testing-your-first-application/installing-cypress-and-writing-our-first-test")
         
     })
 })
